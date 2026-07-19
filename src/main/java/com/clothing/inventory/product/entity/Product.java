5429db49315fc99b56554278b6aeafb4be92ecbf
pyushh.3 <piyushranva62@gmail.com>
2026-07-19 09:11:33 +0530

    implementing product module(hold for now)

---
+++ src/main/java/com/clothing/inventory/product/entity/Product.java
@@ -0,0 +1,46 @@
+package com.clothing.inventory.product.entity;
+
+import com.clothing.inventory.category.entity.Category;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.Id;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 500)
+    private String description;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String sku;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private boolean deleted;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+}
